0ec1d0ddff294847c633e4fc59af1a70891a33bf
leksmi <alexdotspb@gmail.com>
2020-10-06 09:45:35 +0300

    task_5_3a has finished

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -24,3 +24,32 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+# Шаблон запроса VLAN в зависимости от режима порта (подставим его далее в input):
+vlan_template = {
+    'access': 'Введите номер VLAN: ',
+    'trunk': 'Введите разрешенные VLANы: '
+}
+
+# Get data from user:
+int_mode = input('Введите режим работы интерфейса (access/trunk): ')
+int_numb = input('Введите тип и номер интерфейса: ')
+vlan_s = input(vlan_template[int_mode])
+
+int_templates = {
+    'access': [
+        "switchport mode access",
+        "switchport access vlan {}".format(vlan_s),
+        "switchport nonegotiate",
+        "spanning-tree portfast",
+        "spanning-tree bpduguard enable"
+        ],
+    'trunk': [
+        "switchport trunk encapsulation dot1q",
+        "switchport mode trunk",
+        "switchport trunk allowed vlan {}".format(vlan_s)
+        ]
+}
+
+print('\n')
+print(f'interface {int_numb}', '\n'.join(int_templates[int_mode]), sep='\n')
