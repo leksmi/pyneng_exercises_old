8b695c038e2a6018e4b142fd83a1d006bbd26f59
leksmi <alexdotspb@gmail.com>
2020-10-17 19:48:12 +0300

    task_6_2a and 6_3 are finished

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,30 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+ip_add = input('Enter an ip address: ').split('.')
+
+if len(ip_add) != 4:
+    print('Неправильный IP-адрес !')
+    exit(1)
+for oct in ip_add:
+    if not oct.isdigit():
+        print('Неправильный IP-адрес !')
+        exit(1)
+    if (int(oct) < 0 or int(oct) > 255):
+        print('Неправильный IP-адрес !')
+        exit(1)
+
+octs_sum = int(ip_add[0]) + int(ip_add[1]) + int(ip_add[2]) + int(ip_add[3])
+
+if octs_sum == 0:   # check for sum 0
+    print('The ip address is *unassigned* :', '.'.join(ip_add))
+elif octs_sum == 1020:  # check for sum 1020
+    print('The ip address is *local broadcast* :', '.'.join(ip_add))
+elif (int(ip_add[0]) >= 1) and  (int(ip_add[0]) <= 223):
+    print('The ip address is *unicast* :', '.'.join(ip_add))
+elif (int(ip_add[0]) >= 224) and  (int(ip_add[0]) <= 239):
+    print('The ip address is *multicast* :', '.'.join(ip_add))
+else:
+    print('The ip address is *unused* :', '.'.join(ip_add))
+
--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -44,10 +44,27 @@ trunk_template = [
 access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
 trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
 
+#
+print('Access interfaces:')
 for intf, vlan in access.items():
     print("interface FastEthernet" + intf)
     for command in access_template:
         if command.endswith("access vlan"):
             print(" {} {}".format(command, vlan))
+        else:
+            print(" {}".format(command))
+#
+print('\nTrunk interfaces:')
+for intf, vlan in trunk.items():
+    print(f'interface FastEthernet {intf}')
+    for command in trunk_template:
+        if command.endswith('allowed vlan'):
+            vlans = ','.join(vlan[1:])
+            if vlan[0] == 'only':
+                print(f' {command} {vlans}')
+            elif vlan[0] == 'del':
+                print(f' {command} remove {vlans}')
+            elif vlan[0] == 'add':
+                print(f' {command} add {vlans}')
         else:
             print(" {}".format(command))
