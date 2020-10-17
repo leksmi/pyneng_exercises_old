2fec7b4ef80651eca1a47da3faf613ca5c477e32
leksmi <alexdotspb@gmail.com>
2020-10-17 11:49:48 +0300

    task_6_1 and 6_2 are finished

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -12,3 +12,15 @@
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+
+mac_cisco = []
+for i in range(len(mac)):
+    mac_cisco.append(mac[i].replace(':', '.'))
+
+print(mac_cisco)
+
+
+
+
+
+
--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -14,3 +14,18 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_add = input('Enter an ip address: ').split('.')
+
+octs_sum = int(ip_add[0]) + int(ip_add[1]) + int(ip_add[2]) + int(ip_add[3])
+
+if octs_sum == 0:   # check for sum 0
+    print('The ip address is *unassigned* :', '.'.join(ip_add))
+elif octs_sum == 1020:  # check for sum 1020
+    print('The ip address is *local broadcast* :', '.'.join(ip_add))
+elif (int(ip_add[0]) >= 1) and  (int(ip_add[0]) <= 223):
+    print('The ip address is *unicast* :', '.'.join(ip_add))
+elif (int(ip_add[0]) >= 224) and  (int(ip_add[0]) <= 239):
+    print('The ip address is *multicast* :', '.'.join(ip_add))
+else:
+    print('The ip address is *unused* :', '.'.join(ip_add))
