65322b0761669176b9f5df98b02ad37ce673fcfc
leksmi <alexdotspb@gmail.com>
2021-03-26 13:02:56 +0300

    task_12_1 finished

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -3,7 +3,6 @@
 Задание 12.1
 
 Создать функцию ping_ip_addresses, которая проверяет пингуются ли IP-адреса.
-
 Функция ожидает как аргумент список IP-адресов.
 
 Функция должна возвращать кортеж с двумя списками:
@@ -14,3 +13,21 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import subprocess
+
+def ping_ip_addresses(ip_list):
+    reach_ip = []
+    unreach_ip = []
+    for ip_add in ip_list:
+        result = subprocess.run(f'ping -n -c 3 {ip_add}'.split(), stdout=subprocess.PIPE, encoding='utf-8')
+        if 'ttl' in result.stdout.lower():
+            reach_ip.append(ip_add)
+        else:
+            unreach_ip.append(ip_add)
+    # print(f'Reachable IPs : {reach_ip}', f'\nUnreachable IPs : {unreach_ip}')
+    return reach_ip, unreach_ip
+
+ip_addresses = ['10.252.136.210', '10.252.136.211', '10.252.136.212', '10.252.136.213', '10.252.136.214', '10.252.136.215']
+
+print(ping_ip_addresses(ip_addresses))
